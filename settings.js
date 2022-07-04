27d26ea40c4400da030bcc0a753c693920109696
manuthlakdiv <108653910+manuthlakdiv@users.noreply.github.com>
2022-07-04 12:41:36 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,9 +22,9 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94705763890'] //Owner number in aive msg
+global.premium = ['+94705763890'] //Owner Number info
+global.ownernomer = '+95705763890' //Owner Number <<<
 
 global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
 global.botname = ' ☬RED༒DRAGON' //Bot Name
@@ -34,13 +34,13 @@ global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = '☬🔒💚𝙶𝚎𝚗𝚊𝚛𝚊𝚝𝚎𝚍 𝙱𝚢 ᴹᴿ.𝙼𝚊𝚗𝚞𝚝𝚑💜🔒࿐' //Bot Pacage Name
+global.author = '😎L.K.H.Manuth Lakdiv❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '©_Manu.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
 
